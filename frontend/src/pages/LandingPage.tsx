a4ae0843d67de946979c00d6ad652eb4f7576aa1
Thykp <101046365+Thykp@users.noreply.github.com>
2025-09-08 00:19:51 +0800

    feat: added React-Router-Dom for SPA routing

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button"
+
+export default function LandingPage() {
+    return (
+        <Button>Welcome to the Landing Page for our project!</Button>
+    )
+}
