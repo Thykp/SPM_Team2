f64ed812ec999ef993c08a88cf927a40fbc810f0
YF Leong <yfbochap@gmail.com>
2025-09-16 17:15:17 +0800

    fix: updated unit test for task ms
    - Moved task-get-all.test.js to different test folder
    - Updated task-post-new.test.js

--- backend/services/atomic/task/test/SPG-23/task-post-new.test.js
+++ backend/services/atomic/task/test/SPG-23/task-post-new.test.js
@@ -7,7 +7,10 @@ describe('POST /task/new', () => {
       title: "New Task",
       deadline: "2025-09-30",
       description: "This is a test task",
-      status: "pending"
+      status: "pending",
+      collaborators: ["588fb335-9986-4c93-872e-6ef103c97f92", "c0cd847d-8c61-45dd-8dda-ecffe214943e"],
+      owner: "c0cd847d-8c61-45dd-8dda-ecffe214943e",
+      parent: "e8f89b64-84db-4d69-9b19-f21fcc1565a9"
     };
 
     const res = await request(app)
