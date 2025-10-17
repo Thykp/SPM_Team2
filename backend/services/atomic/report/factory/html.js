44c472138f36b041da09cd79932df2defa65b013
WongYH <wyhui@outlook.com>
2025-10-17 09:01:33 +0800

    fix: Edited dockerfile to fix half-rendered images in report

--- backend/services/atomic/report/factory/html.js
+++ backend/services/atomic/report/factory/html.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const nunjucks = require('nunjucks');
 
-const TEMPLATE_PATH = path.resolve(__dirname, 'personalReport.njk');
+// const TEMPLATE_PATH = path.resolve(__dirname, 'personalReport.njk');
 
 // Configure Nunjucks to look in the current directory
-const env = nunjucks.configure(path.dirname(TEMPLATE_PATH), { autoescape: true });
+const env = nunjucks.configure('./templates', { autoescape: true });
 
 async function renderHtml({ userId, tasks, kpis, reportPeriod, charts, template_file }) {
   return env.render(template_file, {
